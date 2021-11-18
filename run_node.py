98c8f1855b039aa964bcbce40f5565d2a1ec5831
Sasha Popov <aleksandrspopov812@gmail.com>
2021-11-17 21:20:20 -0500

    Fully working training

--- run_node.py
+++ run_node.py
@@ -3,14 +3,12 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Distributed ML with SGX node')
 
-parser.add_argument('-np','--node_port', help='Port of this node', required=True)
-parser.add_argument('-sa','--server_address', help='Address of the server', required=True)
-parser.add_argument('-sp','--server_port', help='Port of the server', required=True)
-
-parser.add_argument('-p','--package_file', help='JSON file containing packages needed', required=True)
+parser.add_argument('-np','--node_port', help='Port of this node', required=True, type=int)
+parser.add_argument('-sa','--server_address', help='Address of the server', required=True, type=str)
+parser.add_argument('-sp','--server_port', help='Port of the server', required=True, type=int)
 
 args = vars(parser.parse_args())
 
 if __name__ == "__main__":
 
-	run_node('127.0.0.1', args['node_port'], args['server_addres'], args['server_port'])
+	run_node('127.0.0.1', args['node_port'], args['server_address'], args['server_port'])
--- run_server.py
+++ run_server.py
@@ -7,11 +7,10 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 import torch.optim as optim
 import torch.nn.functional as F
+import torch.nn as nn
 from tqdm import tqdm
 
 from DistributedSGX.server import distributed_sgx
-
-from model import Net
 
 @distributed_sgx(num_nodes=1)
 def train(dataloader, device, model, optimizer):
@@ -27,6 +26,30 @@ def train(dataloader, device, model, optimizer):
 
 	return True
 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
 def main():
 
